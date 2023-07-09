964955f34d746d4bfde49eb589f69fd895ef6f9c
Elia Partin <arenaselia@gmail.com>
2023-07-08 20:55:47 -0400

    added login views

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,7 +13,32 @@ class HomeView(APIView):
         content = {'message': 'Welcome to the Aunthentication page!'}
         return Response(content)
    
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
 
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                'refresh_token': str(refresh),
+                'access_token': str(refresh.access_token),
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+   
+# class LoginView(APIView):
+#     def post(self, request):
+#         # Retrieve the username and password from the request body
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         return JsonResponse({'message': 'Login successful'})
+
+#     def http_method_not_allowed(self, request):
+#         return JsonResponse({'error': 'Method not allowed'}, status=405)
     
 class LogoutView(APIView):
      permission_classes = (IsAuthenticated,)

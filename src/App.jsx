4bba93fc106ad73bd54af16975b46d9a3f3feb0c
Elia Partin <arenaselia@gmail.com>
2023-07-09 11:54:55 -0400

    added main

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import UserLogin from "./login";
+import { BrowserRouter, Route} from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      
+      <h1>Welcome to Meal Master</h1>
+      
+      {/* <BrowserRouter /> */}
+      {/* <Route path="/home"></Route> */}
+
+      <UserLogin />
+      <div></div>
+    </>
+  );
+}
+
+export default App;
